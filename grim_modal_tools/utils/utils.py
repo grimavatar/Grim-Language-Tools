c32fcd5637f5def5c758ff648a330e800a89b76b
grimavatar <57611284+grimavatar@users.noreply.github.com>
2026-04-26 08:15:42 -0400

    Update utils.py

--- grim_modal_tools/utils/utils.py
+++ grim_modal_tools/utils/utils.py
@@ -1,3 +1,6 @@
+import shutil
+from pathlib import Path
+
 
 def suppress_logging():
     # Suppress standard Python logging warnings
@@ -8,3 +11,18 @@ def suppress_logging():
     # # Suppress NeMo's verbose Info (I) and Warning (W) logs
     # import nemo.utils as nemo_utils
     # nemo_utils.logging.setLevel(nemo_utils.logging.ERROR)
+
+
+def compress(output_path: str | Path) -> None:
+    if isinstance(output_path, str):
+        output_path = Path(output_path)
+
+    is_empty = not any(output_path.iterdir())
+    if is_empty:
+        return
+
+    shutil.make_archive(
+        base_name = output_path,  # .zip is added automatically
+        format = "zip", 
+        root_dir = output_path
+    )
